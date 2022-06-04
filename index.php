01c0ee78dce0e4b32eda03ae911ae8b7ef9aad9e
Aanchi-glitch2744 <aanchalkumari27444@gmail.com>
2022-06-04 11:55:28 +0530

    Added method to login signup using database

--- index.php
+++ index.php
@@ -6,12 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat App - Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <div class="w-400 p-5 rounded"></div>
+<body class="d-flex justify-content-center align-items-center vh-100">
+    <div class="w-400 p-5 shadow rounded">
+        <form method="post" action="app/http/auth.php">
+            <div class="d-flex justify-content-center align-items-center flex-column">
+            <img src="img/logo.png" class="w-25">
+            <h3 class="display-4 fs-1 text-center">LOGIN</h3>
+            </div>
+            <?php if (isset($_GET['success'])) { ?>
+                <div class="alert alert-warning" role="alert"> <?php echo htmlspecialchars($_GET['success']);?> </div>
+                <?php } ?>
+            <div class="mb-3">
+                <label class="form-label">User Name</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control">
+            </div>
+          <button type="submit" class="btn btn-primary"> LOGIN </button>
+          <a href="signup.php">Sign Up</a>
+        </form>
+    </div>
+    
     
 </body>
 </html>
 
 
-<!-- 5:18 -->
+<!-- 35:28 -->
