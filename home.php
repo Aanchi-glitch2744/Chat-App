d4474a7ed3b5f6824678088e1f9dcc3c65b327d3
Aanchi-glitch2744 <aanchalkumari27444@gmail.com>
2022-06-07 12:25:16 +0530

    User search list completed wit active display

--- home.php
+++ home.php
@@ -6,12 +6,20 @@
     # database connection file
   	include 'app/db.conn.php';
     include 'app/helpers/user.php';
+    include 'app/helpers/conversations.php';
+    include 'app/helpers/timeAgo.php';
+
 
     # Getting User data data
   	$user = getUser($_SESSION['username'], $conn);
-
     // echo "<pre>";
     // print_r($user);
+
+    # Getting User conversations
+  	$conversations = getConversation($user['user_id'], $conn);
+    // print_r($conversations);
+
+    
 
 ?>
 
@@ -23,9 +31,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat App - Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" href="img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="/css/style.css"> -->
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="p-2 w-400 rounded shadow">
@@ -33,9 +42,7 @@
         <div class="d-flex mb-3 p-3 bg-light justify-content-between align-items-center">
             <div class="d-flex align-items-center">
               <img src="uploads/<?=$user['p_p']?>" class="w-25 rounded-circle">
-              <!-- <img src="uploads/user-default.png" class="w-25 rounded-circle"> -->
-              <!-- <h3 class="fs-xs m-2">Name</h3>  -->
-              <h3 class="fs-xs m-2"><?=$user['name']?></h3>
+              <h3 style="font-size: 1rem;" class="fs-xs m-2"><?=$user['name']?></h3>
                   
             </div>
           <a href="logout.php" class="btn btn-dark">Logout</a>
@@ -43,12 +50,35 @@
         <div class="input-group mb-3">
     			<input type="text" placeholder="Search..." id="searchText" class="form-control">
     			<button class="btn btn-primary" id="serachBtn">
-    			        <i class="fa fa-search"></i>
-                  
-                  
-    			</button>       
+    			        <i class="fa fa-search"></i>       
+    			</button>        
     		</div> 
-      
+        <ul id="chatList" class="list-group mvh-50 over">
+          <?php if (!empty($conversations)) { ?>
+          <?php foreach ($conversations as $conversation){ ?>
+
+          <li class="list-group-item">
+            <a href="chat.php?user=<?=$conversation['username']?>" class="justify-content-between align-items-center p-2">
+            <div class="d-flex align-items-center">
+                <img style="width: 10%;" src="uploads/<?=$conversation['p_p']?>" class="w-10 rounded-circle">
+                <h3 style="font-size: 1rem;" class="fs-xs m-2"><?=$conversation['name']?></h3>
+            </div>
+            <?php if (last_seen($conversation['last_seen']) == "Active") { ?>
+		    					<div title="online">
+		    						<div class="online"></div>
+		    					</div>
+	    					<?php } ?>
+            </a>
+          </li>
+          <?php } ?>
+          <?php }else{ ?>
+    				<div class="alert alert-info
+    				            text-center">
+					   <i style="font-size: 5rem;" class="fa fa-comments d-block fs-big"></i>
+                       No messages yet, Start the conversation
+					  </div>     
+          <?php } ?>
+        </ul>
       </div>
 
     </div>
