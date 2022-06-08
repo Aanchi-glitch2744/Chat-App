265f4822c493580ba8868e005eb65c4911065f91
Aanchi-glitch2744 <aanchalkumari27444@gmail.com>
2022-06-08 10:38:24 +0530

    Implemented search using ajax

--- home.php
+++ home.php
@@ -31,10 +31,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat App - Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- <link rel="stylesheet" href="/css/style.css"> -->
+    <style>
+   <?php include "css/style.css" ?> 
+    </style>
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="p-2 w-400 rounded shadow">
@@ -60,8 +62,8 @@
           <li class="list-group-item">
             <a href="chat.php?user=<?=$conversation['username']?>" class="justify-content-between align-items-center p-2">
             <div class="d-flex align-items-center">
-                <img style="width: 10%;" src="uploads/<?=$conversation['p_p']?>" class="w-10 rounded-circle">
-                <h3 style="font-size: 1rem;" class="fs-xs m-2"><?=$conversation['name']?></h3>
+                <img src="uploads/<?=$conversation['p_p']?>" class="w-10 rounded-circle">
+                <h3 class="fs-xs m-2"><?=$conversation['name']?></h3>
             </div>
             <?php if (last_seen($conversation['last_seen']) == "Active") { ?>
 		    					<div title="online">
@@ -86,7 +88,58 @@
 
 
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+      $(document).ready(function(){
+      
+      // Search
+       $("#searchText").on("input", function(){
+       	 var searchText = $(this).val();
+          // console.log(searchText);
+         if(searchText == "") return;
+         $.post('app/ajax/search.php', 
+         	     {
+         	     	key: searchText
+         	     },
+         	   function(data, status){
+                  $("#chatList").html(data);
+         	   });
+       });
 
+       // Search using the button
+       $("#serachBtn").on("click", function(){
+       	 var searchText = $("#searchText").val();
+         if(searchText == "") return;
+         $.post('app/ajax/search.php', 
+         	     {
+         	     	key: searchText
+         	     },
+         	   function(data, status){
+                  $("#chatList").html(data);
+         	   });
+       });
+
+
+      /** 
+      auto update last seen 
+      for logged in user
+      **/
+      let lastSeenUpdate = function(){
+      	$.get("app/ajax/update_last_seen.php");
+        // $.get("app/ajax/update_last_seen.php", function(data, status){
+        //   console.log(data);
+        // });
+      }
+      lastSeenUpdate();
+      /** 
+      auto update last seen 
+      every 10 sec
+      **/
+      setInterval(lastSeenUpdate, 10000);
+
+    });
+
+    </script>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -11,8 +11,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat App - Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="img/logo.png">
+    <style>
+   <?php include "css/style.css" ?> 
+    </style>
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="w-400 p-5 shadow rounded">
@@ -50,4 +53,4 @@
   }
  ?>
 
-<!-- 47:01 -->
+<!-- 1:25:20 -->
